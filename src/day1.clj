c6e1f5932a185df66bdfb0a413805acf8b7e9100
James Liu <james@jamesliu.io>
2021-12-05 11:38:44 +1100

    Day 1, puzzle 2

--- src/day1.clj
+++ src/day1.clj
@@ -10,12 +10,32 @@
        (str/split-lines)
        (map edn/read-string)))
 
-(defn run
-  [opts]
+(defn sum [coll]
+  (reduce + coll))
+
+(def part-1
   (->> measurements
        (partition 2 1)
        (map (fn [pair]
               (cond
                 (> (last pair) (first pair)) 1
                 :else 0)))
-       (reduce +)))
+       (sum)))
+
+(def sliding-window
+  (->> measurements
+       (partition 3 1)))
+
+(def part-2
+  (->> sliding-window
+       (partition 2 1)
+       (map (fn [pair]
+              (cond
+                (> (sum (last pair)) (sum (first pair))) 1
+                :else 0)))
+       (sum)))
+
+(defn run
+  [opts]
+  (println part-1)
+  (println part-2))
