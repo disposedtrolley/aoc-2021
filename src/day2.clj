66f3673590ac9831556c535eafb30049a7097dd9
James Liu <james@jamesliu.io>
2021-12-05 12:47:04 +1100

    Day 2, puzzle 2

--- src/day2.clj
+++ src/day2.clj
@@ -41,6 +41,43 @@
             course)
     (reduce *)))
 
+;; Part 2 introduces more complex rules for calculating how much to
+;; change the depth and horizontal position by. To make this cleaner,
+;; the functions have been reorganised to process specific directions
+;; and mutate the position hash, rather than only computing the
+;; horizontal position and depth.
+;;
+;; This would probably work just as well for Puzzle 1, but I'll keep
+;; the solution as-is so we can compare and contrast the changes.
+(defn process-forward
+  [{horizontal :horizontal depth :depth aim :aim :as position} units]
+  (assoc position
+    :horizontal (+ horizontal units)
+    :depth (+ depth (* aim units))))
+
+(defn process-up
+  [{aim :aim :as position} units]
+  (assoc position
+    :aim (- aim units)))
+
+(defn process-down
+  [{aim :aim :as position} units]
+  (assoc position
+    :aim (+ aim units)))
+
+(def part-2
+  (let [position
+        (reduce (fn [acc
+                     [direction units :as step]]
+                  (cond (= "up" direction) (process-up acc units)
+                        (= "down" direction) (process-down acc units)
+                        (= "forward" direction) (process-forward acc units)
+                        :else acc))
+                {:horizontal 0 :depth 0 :aim 0}
+                course)]
+    (* (position :horizontal) (position :depth))))
+
 (defn run
   [opts]
-  (println part-1))
+  (println part-1)
+  (println part-2))
